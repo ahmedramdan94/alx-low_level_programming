ca87dabfae35932549374d42d9d94bc50c0b667b
ahmedramdan94 <bezorezo58@gmail.com>
2023-08-10 14:35:35 -0700

    size of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this is  main function print to user types size
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+printf("Size of a char: %u byte(s)\n", sizeof(char));
+printf("Size of a int: %u byte(s)\n", sizeof(int));
+printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+printf("Size of a float: %u byte(s)\n", sizeof(float));
+
+return (0);
+
+}
+
