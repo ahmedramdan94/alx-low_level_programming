f542cefa5cd430a9f03b31c73b622943597507f4
ahmedramdan94 <bezorezo58@gmail.com>
2023-08-11 16:11:20 -0700

    reveres alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * Description: print reverse alphabet in lowercase
+ * Return: alawys (0)
+ */
+int main(void)
+{
+char alpha = 'z';
+while (alpha <= 'a')
+{
+putchar(alpha);
+alpha--;
+
+}
+
+putchar('\n');
+
+return (0);
+}
