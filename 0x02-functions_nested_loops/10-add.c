8c03bdedf7e9830830afac238be53dd9d66de29f
ahmedramdan94 <bezorezo58@gmail.com>
2023-08-15 14:30:02 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - returns resulet of add two arrg
+ * @a: int type number
+ * @b: int type number
+ * Return:0
+ */
+
+
+int add(int a, int b)
+{
+	int resulet = a + b;
+
+return (resulet);
+}
+
