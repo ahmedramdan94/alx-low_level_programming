b3be16201bb884b3e71fcfd09ee9453034158519
ahmedramdan94 <bezorezo58@gmail.com>
2023-08-15 11:08:10 -0700

    alaphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,8 +9,8 @@ void print_alphabet(void)
 {
 
 int alpha;
-for (alpha = 'a'; i < 'z'; alpha++)
+for (alpha = 'a'; alpha < 'z'; alpha++)
 	_putchar(alpha);
 _putchar('\n');
-return (0);
+
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
-#include <stdio.h>
 
 int _putchar(char c);
 void print_alphabet(void);
