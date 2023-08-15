bb21688c3b38ad1c1697fb094f98ee5eb37eb63a
ahmedramdan94 <bezorezo58@gmail.com>
2023-08-15 10:24:20 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - main functions
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+char c[9] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+	_putchar(c[i]);
+_putchar('\n');
+return (0);
+}
