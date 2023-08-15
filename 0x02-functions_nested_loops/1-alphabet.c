1458716aaca410eb7673fb257a7f43af610327e6
ahmedramdan94 <bezorezo58@gmail.com>
2023-08-15 10:36:58 -0700

    alaphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make func to lowercase alphabet from a to z
+ * Return: Always (0)
+ */
+
+void print_alphabet(void)
+{
+
+int alpha;
+for (alpha = 'a'; i < 'z'; alpha++)
+	_putchar(alpha);
+_putchar('\n');
+return (0);
+}
