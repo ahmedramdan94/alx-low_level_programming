e5c94a39bf35187283ed25fd9db1c64ce0f973b3
ahmedramdan94 <bezorezo58@gmail.com>
2023-08-17 13:06:51 -0700

    nums speak

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - prints nums from 0 to 9
+ */
+
+void print_numbers(void)
+{
+char g = 0;
+
+	while (g <= 9)
+	{
+		_putchar('0' + g);
+		g++;
+
+	}
+	_putchar('\n');
+
+}
