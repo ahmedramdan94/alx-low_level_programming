6bfab530efce207404b094dd91bfc87096cf2992
ahmedramdan94 <bezorezo58@gmail.com>
2023-08-17 12:20:44 -0700

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multi two num
+ * @a: int type number
+ * @b: int type number
+ * Return:resulet
+ */
+
+
+int mul(int a, int b)
+{
+	int resulet = a * b;
+return (resulet);
+}
