385be8ce6a9c664dfd3c72d2aeadf7fc088df3b9
ahmedramdan94 <bezorezo58@gmail.com>
2023-09-01 08:52:11 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function print program name
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
